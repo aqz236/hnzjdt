4f03fb528301cb993fe6598205eba42de4649632
程序圆 <54022108+aqz236@users.noreply.github.com>
2021-11-29 01:45:52 +0800

    模糊匹配  v1.5

--- tool/fuzz.py
+++ tool/fuzz.py
@@ -9,31 +9,31 @@ def simpleMatching(title,tiku,option):
         like = fuzz.ratio(title, i)
         #题目相似度
         if like >68:
-            #如果遇到题目相似，答案不同。  做相似度对比 取最大相似度对应的信息，
+   
             checkList[i] = like
         else:
             pass
     try:
-        #一个题目匹配到多个答案就检查答案存在否
         for oo in checkList:
             checkAns = tiku[oo]
             if checkAns[0] in option:
                 return [oo, like, checkAns]
-        # checkedTitle = max(checkList, key=checkList.get)
     except:
         pass
+
 #数据对撞
 def dataCollision(ansList, quesList, title, tiku):
     #多选
     quesDic = {}
     xuanxiang = ["A","B","C","D"]
     num = 0
+    #给这道题选项编号 {"A":"选项1","B":"选项2"}
     for i in quesList:
         quesDic[xuanxiang[num]] = i
         num+=1
+    print("看一下此题选项列表:",quesDic)
     if len(ansList) > 1:
         rightList = dataCollision2(ansList, quesDic, title, tiku, quesList)
-        print("rightList::",rightList)
         return ','.join(set(rightList))
     #单选
     elif len(ansList) == 1:
@@ -67,43 +67,60 @@ def dataCollision1(ansList,quesList):
 #多选
 def dataCollision2(ansList, quesDic, title, tiku, quesList):
     # # ansList = ['真实的', '虚幻的']
-    # # quesDic =  {'A': ' 真实的 ', 'B': '半真半假的 ', 'C': '虚幻的 '}
+    # # quesDic =  {'A': ' 真实的 ', 'B': '半真半假的 ', 'C': '虚幻的 '} 这题的乱序选项字典
     # title 这题的题目
+    #tike 题库
+    #quesList 这题的乱序选项
     checkList = {}
+
     for i in tiku:
-        titleLike = fuzz.ratio(title, i)  # 题目相似度
+        titleLike = fuzz.ratio(title, i)  # 在题库中匹配答案
+        #如果匹配到了即完美匹配 就返回
         if titleLike == 100:
-            checkList[i] = 100# 完美匹配
-            print("完美匹配")
-            rightAnswerList = tiku[max((checkList))]#取答案列表
+            daanliebiao = tiku[i]#拿到答案列表
             rightKeyList = []
-            #答案匹配
-            for ii in quesDic:
-                for pp in rightAnswerList:
-                    like = fuzz.ratio(pp, quesDic[ii])#答案相似度
-                    if like > 88:
-                        rightKeyList.append(ii)
-            checkList.clear()
+            #选项进行排序
+            for iii in quesDic:
+                for ppp in daanliebiao:
+                    like = fuzz.ratio(ppp, quesDic[iii])  # 答案相似度
+                    if like > 88:#得出选项列表['A','B','C']
+                        rightKeyList.append(iii)
             return rightKeyList
 
-        if titleLike > 60 and titleLike != 100: #题目较为相似
-            # 双重匹配  开始匹配答案相似度
+        #非完美匹配  双重匹配
+        elif titleLike > 60 and titleLike != 100:#题目比较相似
+            #对答案进行匹配
             quesDicToList = []
             for item in quesDic:
                 quesDicToList.append(quesDic[item])
-            answerLike = fuzz.ratio(','.join(set(ansList)), ','.join(set(quesDicToList)))
-            if answerLike > 45:
-                #数据对撞
-                rightKeyList = []
-                for ii in quesDic:
-                    for pp in ansList:
-                        like = fuzz.ratio(pp, quesDic[ii])
-                        if like > 88:
-                            rightKeyList.append(ii)
-                return rightKeyList
-        else:
-            pass
+            answerLike = fuzz.ratio(','.join(set(ansList)), ','.join(set(quesDicToList)))#答案整体相似度
+            if answerLike > 35:
+                doubleLike = titleLike + answerLike
+                checkList[i] = doubleLike
+
+        elif titleLike > 60 and titleLike != 100:  # 题目比较相似
+            allLikeValue = {}
+            addLikeValue = 0
+            # 双重匹配  开始匹配答案相似度
+            quesDicToList = []
+            for item in quesDic:#字典转列表
+                quesDicToList.append(quesDic[item])
+            answerLike = fuzz.ratio(','.join(set(ansList)), ','.join(set(quesDicToList)))  # 答案整体相似度
+            addLikeValue = titleLike + answerLike
+            checkList[i] = addLikeValue  # 题目对应总相似度
 
 
+    # print('最大', tiku[max((checkList))])#筛选出组合相似度最大的答案选项
+    rightKeyList = []
+    # 选项进行排序
+    try:
+        maxCheckList = tiku[max((checkList))]
+        for iii in quesDic:
+            for ppp in maxCheckList:
+                like = fuzz.ratio(ppp, quesDic[iii])  # 答案相似度
+                if like > 88:  # 得出选项列表['A','B','C']
+                    rightKeyList.append(iii)
+        return rightKeyList
+    except:
+        return ['A','B']
 
- 
