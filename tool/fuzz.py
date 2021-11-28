7945d4b6762fef9d9f78a71d23203ef1fe0a02c8
程序圆 <54022108+aqz236@users.noreply.github.com>
2021-11-28 17:50:52 +0800

    Update fuzz.py
    重要更新，防止多选时返回控制导致无法提交

--- tool/fuzz.py
+++ tool/fuzz.py
@@ -10,9 +10,6 @@ def simpleMatching(title,tiku,option):
         #题目相似度
         if like >68:
             #如果遇到题目相似，答案不同。  做相似度对比 取最大相似度对应的信息，
-            #例如遇到题目为：下面说法正确的是:()。
-            # 这样的就需要再写逻辑进行对比 比如选项模糊匹配累计相似度
-            #即答案与答案匹配，有时间再写吧
             checkList[i] = like
         else:
             pass
@@ -38,6 +35,7 @@ def dataCollision(ansList, quesList, title, tiku):
     print("看一下此题选项列表:",quesDic)
     if len(ansList) > 1:
         rightList = dataCollision2(ansList, quesDic, title, tiku, quesList)
+        print("rightList::",rightList)
         return ','.join(set(rightList))
     #单选
     elif len(ansList) == 1:
@@ -77,32 +75,44 @@ def dataCollision2(ansList, quesDic, title, tiku, quesList):
     for i in tiku:
         titleLike = fuzz.ratio(title, i)  # 题目相似度
         if titleLike == 100:
-            checkList[i] = 100
-        if titleLike > 70 & titleLike != 100:
-            # print("通过题目匹配，题目相似度：%s" % titleLike)
+            checkList[i] = 100# 完美匹配
+            print("完美匹配")
+            rightAnswerList = tiku[max((checkList))]#取答案列表
+            rightKeyList = []
+            #答案匹配
+            for ii in quesDic:
+                for pp in rightAnswerList:
+                    like = fuzz.ratio(pp, quesDic[ii])#答案相似度
+                    if like > 88:
+                        rightKeyList.append(ii)
+            checkList.clear()
+            return rightKeyList
+
+        if titleLike > 60 and titleLike != 100: #题目较为相似
             # 双重匹配  开始匹配答案相似度
-            answerLike = fuzz.ratio(','.join(set(ansList)), ','.join(set(quesDic)))
-            if answerLike > 25:
-                # print("通过答案匹配，答案相似度：%s" % answerLike)
-                doubleLike = titleLike + answerLike
-                checkList[i] = doubleLike
+            quesDicToList = []
+            for item in quesDic:
+                quesDicToList.append(quesDic[item])
+            answerLike = fuzz.ratio(','.join(set(ansList)), ','.join(set(quesDicToList)))
+            print("答案相似度",answerLike)
+            print(ansList)
+            print(quesDicToList)
+            if answerLike > 45:
+                #数据对撞
+                rightKeyList = []
+                for ii in quesDic:
+                    for pp in ansList:
+                        like = fuzz.ratio(pp, quesDic[ii])
+                        if like > 88:
+                            rightKeyList.append(ii)
+                return rightKeyList
+
             else:
-                doubleLike = titleLike + 0
-                checkList[i] = doubleLike
+                #正常是到不了这里的，如果题库更新 防止找不到题目就默认返回[A,B,C,D]
+                rightKeyList = ["A","B","C","D"]
+                return rightKeyList
         else:
             pass
-
-    rightAnswerList = tiku[max((checkList))]
-
-
-    rightKeyList = []
-    for ii in quesDic:
-        for pp in rightAnswerList:
-            like = fuzz.ratio(pp, quesDic[ii])
-            if like > 88:
-                rightKeyList.append(ii)
-    return rightKeyList
-
 
 
 
