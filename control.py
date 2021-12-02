b651716ac141e0d13938136b3482dfd388442174
F兽 <54022108+aqz236@users.noreply.github.com>
2021-12-02 14:07:49 +0800

    入口文件
    加入了更新提示

--- control.py
+++ control.py
@@ -1,7 +1,9 @@
 #coding=utf-8
 from fuzzywuzzy import fuzz
 import tool.driver as driver
-import requests,json
+import requests,json,webbrowser
+import os
+
 info = {
     "题库": {},
     "学校信息": {},
@@ -12,7 +14,7 @@ info = {
 def getInfo():
     global tiku,schoolInfo
     tikuURL = 'https://blog-static.cnblogs.com/files/FSHOU/tikuData.js'
-    schoolInfoURL = 'https://blog-static.cnblogs.com/files/FSHOU/schoolInfo.js'
+    schoolInfoURL = 'https://blog-static.cnblogs.com/files/FSHOU/schoolInfo2.js'
     tikuInfo = json.loads(requests.get(tikuURL).text.replace("\'","\""))
     info["题库"] = tikuInfo["data"]
     print("题库获取完成 最近更新时间：",tikuInfo["updatetime"])
@@ -21,14 +23,22 @@ def getInfo():
     print("学校信息获取完成, 最近更新时间：", schoolInfos["updatetime"])
     info["最新消息"] = schoolInfos["msg"]
     print(info["最新消息"])
-
+    info["update"] = schoolInfos["update"]
 def run(schoolName,userId,userPasswd):
     code = driver.login(userId,userPasswd,schoolName,info["题库"],info["学校信息"])
     return code
+def logo():
+    print('''  ______   _                   
+     |  ____| | |                  
+     | |__ ___| |__   _____      __
+     |  __/ __| '_ \ / _ \ \ /\ / /
+     | |  \__ \ | | | (_) \ V  V / 
+     |_|  |___/_| |_|\___/ \_/\_/  
 
-if __name__ == "__main__":
-    print("当前版本：1.6")
-    getInfo()
+                                   ''')
+#主要函数
+def main():
+
     while True:
         schoolName = input("请输入学校名称(Enter键入以下一步)：")
         if schoolName not in info["学校信息"]:
@@ -40,7 +50,6 @@ if __name__ == "__main__":
         code = 0
         if code == 1:
             print("执行完成")
-            exit()
         elif code == 2:
             print("学校信息不存在，请手动添加学校学校或者联系作者")
         elif code == 3:
@@ -49,3 +58,38 @@ if __name__ == "__main__":
             pass
 
 
+def createHtml():
+    try:
+        os.remove(f'{userId}国家安全知识考试.html')
+    except:
+        pass
+    with open(f'{userId}国家安全知识考试.html', 'a', encoding='utf-8')as f2:  # f2为文件描述符
+        f2.write(htmlData)
+    webbrowser.open(f'{userId}国家安全知识考试.html')
+    time.sleep(2)
+    os.remove(f'{userId}国家安全知识考试.html')
+if __name__ == "__main__":
+    version = 1.7
+    print(f"当前版本：{str(version)}")
+    logo()#打印logo
+    getInfo()
+    if info["update"]["latestVersion"] > version:
+        print(f"当前程序版本v{str(version)}不是最新版本v",str(info["update"]["latestVersion"]))
+        if info["update"]["level"] == 3:#更新等级 强制更新
+            print("本次更新是重要更新，不更新将程序无法正常使用")
+            print("更新内容：")
+            print(info["update"]["updateMsg"])
+            input("键入Enter跳转项目地址")
+            webbrowser.open("https://github.com/aqz236/hnzjdt")
+        else:
+            print("本次更新非必要更新，下面是更新内容")
+            print(info["update"]["updateMsg"])
+            inputCode = input("输入1忽略更新，输入2跳转项目地址")
+            if inputCode == 1 or inputCode == "1":
+                main()
+            elif inputCode == 2 or inputCode == "2":
+                webbrowser.open("https://github.com/aqz236/hnzjdt")
+    elif info["update"]["latestVersion"] == version:
+        main()
+    else:
+        print("程序出错")
